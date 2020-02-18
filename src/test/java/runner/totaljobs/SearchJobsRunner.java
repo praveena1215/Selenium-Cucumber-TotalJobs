4357f07e9a9e492224f1c9ab17a75c220403a3e5
praveen kumar <praveen_a12@yahoo.com>
2020-02-18 22:49:38 +0000

    With successful cucumber report

--- src/test/java/runner/totaljobs/SearchJobsRunner.java
+++ src/test/java/runner/totaljobs/SearchJobsRunner.java
@@ -6,14 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "features",
-		glue = "stepDef.Totaljobs",
-		tags = {"@clickSearch"},
-		plugin = {"pretty" , "html:target/cucumber-reports","json:target/cucumber.json"}
-		
-		)
-
+@CucumberOptions(features = "features", glue = "stepDef.Totaljobs", tags = { "@clickSearch" }, plugin = { "pretty",
+		"html:target/cucumber", "json:target/cucumber/cucumber.json", "junit:target/cucumber/cucumber.xml" })
 public class SearchJobsRunner {
 
 }
